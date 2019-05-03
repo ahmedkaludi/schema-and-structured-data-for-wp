70d0ec305265ef99348159c077d63abdc31df6ba
Sanjeev Kumar <sanjeevsetu@gmail.com>
2019-05-03 15:56:17 +0530

    #205

--- output/review-output.php
+++ output/review-output.php
@@ -2,8 +2,11 @@
 Class saswp_review_output{
                     
         public function saswp_review_hooks(){
-            add_filter('the_content', array($this, 'saswp_display_review_box'));   
             
+            add_filter('the_content', array($this, 'saswp_display_review_box'));             
+            add_action('wp_head', array($this, 'saswp_display_review_box_schema')); 
+            add_action('amp_post_template_head', array($this, 'saswp_display_review_box_schema')); 
+                                    
         }
         
         public function saswp_review_display_via_shortcode($attr){
@@ -16,6 +19,86 @@ Class saswp_review_output{
               return $result;
               
             }
+            
+        }
+        
+        public function saswp_display_review_box_schema(){
+                          
+                        global $sd_data;
+                        $author_id      = get_the_author_meta('ID');
+											
+			$author_details	= get_avatar_data($author_id);
+			$date 		= get_the_date("Y-m-d\TH:i:s\Z");
+			$modified_date 	= get_the_modified_date("Y-m-d\TH:i:s\Z");
+			$aurthor_name 	= get_the_author();
+                        
+                        if(!$aurthor_name){
+				
+                        $author_id    = get_post_field ('post_author', get_the_ID());
+		        $aurthor_name = get_the_author_meta( 'display_name' , $author_id ); 
+                        
+			}
+                        $saswp_review_details           = esc_sql ( get_post_meta(get_the_ID(), 'saswp_review_details', true));
+                         
+                        $overall_rating   = $saswp_review_details['saswp-review-item-over-all'];
+                        
+                        if($overall_rating && isset($sd_data['saswp-review-module']) && $sd_data['saswp-review-module'] == 1){
+                         
+                            $total_score = esc_attr(number_format((float)$overall_rating, 2, '.', ''));
+                            
+                            $input1 = array(
+                                    '@context'       => 'http://schema.org',
+                                    '@type'          => 'Review',
+                                    'dateCreated'    => esc_html($date),
+                                    'datePublished'  => esc_html($date),
+                                    'dateModified'   => esc_html($modified_date),
+                                    'headline'       => get_the_title(),
+                                    'name'           => get_the_title(),                                    
+                                    'url'            => get_permalink(),
+                                    'description'    => strip_tags(get_the_excerpt()),
+                                    'copyrightYear'  => get_the_time( 'Y' ),                                                                                                           
+                                    'author'	     => array(
+                                                            '@type' 	=> 'Person',
+                                                            'name'		=> esc_attr($aurthor_name),
+                                                            'image'		=> array(
+                                                                    '@type'			=> 'ImageObject',
+                                                                    'url'			=> saswp_remove_warnings($author_details, 'url', 'saswp_string'),
+                                                                    'height'                    => saswp_remove_warnings($author_details, 'height', 'saswp_string'),
+                                                                    'width'			=> saswp_remove_warnings($author_details, 'width', 'saswp_string')
+                                                            ),
+							),                                                        
+                                
+                                    );
+                                    
+                                  $input1['itemReviewed'] = array(
+                                            '@type' => 'Thing',
+                                            'name'  => get_the_title(),
+                                    );
+
+                                    $input1['reviewRating'] = array(
+                                        '@type'       => 'Rating',
+                                        'worstRating' => 1,
+                                        'bestRating'  => 5,
+                                        'ratingValue' => esc_attr($total_score),
+                                        'description' => strip_tags(get_post_meta( get_the_ID(), 'saswp-review-item-description', true )),
+                                     ); 
+                            
+                            if(!empty($input1)){
+                                
+                                echo '<!-- Schema & Structured Data For WP Review Module v'.esc_attr(SASWP_VERSION).' - -->';
+                                echo "\n";
+                                echo '<script type="application/ld+json">'; 
+                                echo "\n";       
+                                echo json_encode($input1);       
+                                echo "\n";
+                                echo '</script>';
+                                echo "\n\n";
+                                
+                            }        
+                            
+                            
+                        }                                         
+            
             
         }
 
