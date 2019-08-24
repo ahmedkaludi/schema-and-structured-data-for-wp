3b4ec0808a74329c7d379757ddcdf6c990e28b41
Sanjeev Kumar <sanjeevsetu@gmail.com>
2019-08-24 13:59:08 +0530

    #355

--- core/array-list/meta_list.php
+++ core/array-list/meta_list.php
@@ -2,55 +2,55 @@
 return array(
                     'text'  => array(                            
                             array(
-                                    'label'     => __( 'Single Element', 'wp-schema-pro' ),
+                                    'label'     => __( 'Single Element', 'schema-and-structured-data-for-wp' ),
                                     'meta-list' => array(
-                                            'blogname'         => __( 'Site Title', 'wp-schema-pro' ),
-                                            'blogdescription'   => __( 'Tagline', 'wp-schema-pro' ),
-                                            'site_url'          => __( 'Site URL', 'wp-schema-pro' ),
-                                            'post_title'        => __( 'Title', 'wp-schema-pro' ),
-                                            'post_content'      => __( 'Content', 'wp-schema-pro' ),
-                                            'post_excerpt'      => __( 'Excerpt', 'wp-schema-pro' ),
-                                            'post_permalink'    => __( 'Permalink', 'wp-schema-pro' ),
-                                            'author_name'       => __( 'Author name', 'wp-schema-pro' ),
-                                            'author_first_name' => __( 'Author first name', 'wp-schema-pro' ),
-                                            'author_last_name'  => __( 'Author last name', 'wp-schema-pro' ),
-                                            'post_date'         => __( 'Publish Date', 'wp-schema-pro' ),
-                                            'post_modified'     => __( 'Last Modify Date', 'wp-schema-pro' ),
+                                            'blogname'         => __( 'Site Title', 'schema-and-structured-data-for-wp' ),
+                                            'blogdescription'   => __( 'Tagline', 'schema-and-structured-data-for-wp' ),
+                                            'site_url'          => __( 'Site URL', 'schema-and-structured-data-for-wp' ),
+                                            'post_title'        => __( 'Title', 'schema-and-structured-data-for-wp' ),
+                                            'post_content'      => __( 'Content', 'schema-and-structured-data-for-wp' ),
+                                            'post_excerpt'      => __( 'Excerpt', 'schema-and-structured-data-for-wp' ),
+                                            'post_permalink'    => __( 'Permalink', 'schema-and-structured-data-for-wp' ),
+                                            'author_name'       => __( 'Author name', 'schema-and-structured-data-for-wp' ),
+                                            'author_first_name' => __( 'Author first name', 'schema-and-structured-data-for-wp' ),
+                                            'author_last_name'  => __( 'Author last name', 'schema-and-structured-data-for-wp' ),
+                                            'post_date'         => __( 'Publish Date', 'schema-and-structured-data-for-wp' ),
+                                            'post_modified'     => __( 'Last Modify Date', 'schema-and-structured-data-for-wp' ),
                                     ),
                             ),
                             array(
-                                    'label'     => __( 'Manual Field', 'wp-schema-pro' ),
+                                    'label'     => __( 'Manual Field', 'schema-and-structured-data-for-wp' ),
                                     'meta-list' => array(
-                                            'manual_text'  => __( 'Manual Text', 'wp-schema-pro' ),                                            
+                                            'manual_text'  => __( 'Manual Text', 'schema-and-structured-data-for-wp' ),                                            
                                     ),
                             ),
                             array(
-                                    'label'     => __( 'Custom Field', 'wp-schema-pro' ),
+                                    'label'     => __( 'Custom Field', 'schema-and-structured-data-for-wp' ),
                                     'meta-list' => array(
-                                            'custom_field' => __( 'Custom Field', 'wp-schema-pro' ),
+                                            'custom_field' => __( 'Custom Field', 'schema-and-structured-data-for-wp' ),
                                     ),
                             ),
                     ),
                     'image' => array(
                             
                             array(
-                                    'label'     => __( 'Single Element', 'wp-schema-pro' ),
+                                    'label'     => __( 'Single Element', 'schema-and-structured-data-for-wp' ),
                                     'meta-list' => array(
-                                            'featured_img' => __( 'Featured image', 'wp-schema-pro' ),
-                                            'author_image' => __( 'Author image', 'wp-schema-pro' ),
-                                            'site_logo' => __( 'Logo', 'wp-schema-pro' ),
+                                            'featured_img' => __( 'Featured image', 'schema-and-structured-data-for-wp' ),
+                                            'author_image' => __( 'Author image', 'schema-and-structured-data-for-wp' ),
+                                            'site_logo' => __( 'Logo', 'schema-and-structured-data-for-wp' ),
                                     ),
                             ),
                             array(
-                                    'label'     => __( 'Manual field', 'wp-schema-pro' ),
+                                    'label'     => __( 'Manual field', 'schema-and-structured-data-for-wp' ),
                                     'meta-list' => array(                                            
-                                            'manual_text'   => __( 'Manual Image URL', 'wp-schema-pro' ),                                           
+                                            'manual_text'   => __( 'Manual Image URL', 'schema-and-structured-data-for-wp' ),                                           
                                     ),
                             ),
                             array(
-                                    'label'     => __( 'Custom Field', 'wp-schema-pro' ),
+                                    'label'     => __( 'Custom Field', 'schema-and-structured-data-for-wp' ),
                                     'meta-list' => array(
-                                            'custom_field' => __( 'Custom Field', 'wp-schema-pro' ),
+                                            'custom_field' => __( 'Custom Field', 'schema-and-structured-data-for-wp' ),
                                     ),
                             ),
                     ),
