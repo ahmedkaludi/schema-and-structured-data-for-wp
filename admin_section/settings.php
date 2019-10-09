a17d57342cc3a2bb0276b4680e2c17f2ba2943aa
sanjeev <sanjeevsetu@gmail.com>
2019-10-09 16:52:30 +0530

    Change log
    Change log

--- admin_section/settings.php
+++ admin_section/settings.php
@@ -480,8 +480,7 @@ function saswp_premium_features_callback(){ ?>
                                         }
                                         
                                         ?> 
-                            
-                            
+                                                        
 				<div class="saswp-features-ele">
                                     <div class="saswp-ele-ic" style="background: #ace;">
                                             <img src="<?php echo SASWP_PLUGIN_URL; ?>/admin_section/images/real-estate-schema-wp.png">
