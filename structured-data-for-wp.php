889c06fee877502c8eaedc25f5e8fff1900e247a
sanjeev <sanjeevsetu@gmail.com>
2019-12-06 12:50:03 +0530

    #638

--- structured-data-for-wp.php
+++ structured-data-for-wp.php
@@ -75,8 +75,8 @@ require_once SASWP_DIR_NAME.'/view/schema_type.php';
 require_once SASWP_DIR_NAME.'/view/paywall.php';  
 require_once SASWP_DIR_NAME.'/admin_section/add-schema/add_new.php';  
 require_once SASWP_DIR_NAME.'/view/post_specific.php';  
-require_once SASWP_DIR_NAME.'/view/review.php';  
-require_once SASWP_DIR_NAME.'/output/review-output.php'; 
+require_once SASWP_DIR_NAME.'/modules/rating-box/backend.php';  
+require_once SASWP_DIR_NAME.'/modules/rating-box/frontend.php'; 
 require_once SASWP_DIR_NAME.'/output/service.php'; 
 require_once SASWP_DIR_NAME.'/output/compatibility.php'; 
 //Loading Reviews files
