94e7c763388292824695786b77a14bbca31005b7
Sanjeevsetu <sanjeevsetu@gmail.com>
2020-06-15 12:11:07 +0530

    #1061

--- output/output.php
+++ output/output.php
@@ -886,13 +886,9 @@ function saswp_schema_output() {
                                     '@id'				=> trailingslashit(saswp_get_permalink()).'#Organization',    
                                     'url'				=> trailingslashit(saswp_get_permalink()),                                                                                    
                                     'description'       => saswp_get_the_excerpt(),                                                                        
-                                    'name'				=> saswp_get_the_title(),			                                                                                                            
-                                    );                                                                                  
-                                                                  
-                                    if(isset($sd_data['saswp_comments_schema']) && $sd_data['saswp_comments_schema'] == 1){
-                                        $input1['comment'] = saswp_get_comments(get_the_ID());
-                                    }
-                                    
+                                    'name'				=> saswp_get_the_title()			                                                                                                            
+                                    );                                                                                                                                                                                        
+                                                                        
                                     $input1 = saswp_append_fetched_reviews($input1, $schema_post_id);
 
                                     $input1 = apply_filters('saswp_modify_organization_schema_output', $input1 ); 
