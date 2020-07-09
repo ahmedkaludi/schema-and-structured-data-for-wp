526f5609e77799726e930176612316760d475ce8
Sanjeevsetu <sanjeevsetu@gmail.com>
2020-07-09 12:01:23 +0530

    #895

--- admin_section/js/structure_admin.js
+++ admin_section/js/structure_admin.js
@@ -45,7 +45,7 @@ var clone = function(){
 			nextId = jQuery(this).parents("tbody").find("tr").length;			
                         selectrow = selectrow.replace(/\[0\]/g, "["+nextId+"]"); 
                         selectrow = selectrow.replace(/\[group-0\]/g, "[group-"+group_index+"]"); 			
-			jQuery(this).parents("tr").after(selectrow);removeHtml();clone();saswp_select2();
+			jQuery(this).parents("tr").after(selectrow);removeHtml();clone();saswp_select2();saswp_schema_datepicker();
 		});
 	}
 	var removeHtml = function(){
@@ -93,35 +93,48 @@ jQuery(document).ready(function($){
            group_index++;
            clone();
 		   removeHtml();
-		   saswp_select2();
+       saswp_select2();
+       saswp_schema_datepicker();
         });
     
 	var selectrow = $("#saswp_amp_select").find("table.widefat tr").html();
 	$("body").append("<script type='template/html' id='call_html_template_sdwp'><tr class='toclone cloneya'>"+selectrow+"</tr>");
 	clone();	
 	removeHtml();
-	saswp_select2();
+  saswp_select2();
+  saswp_schema_datepicker();
 	$(document).on("change", ".select-post-type", function(){
-                var current_change = $(this);
+
+    var current_change = $(this);
 		var parent = $(this).parents('tr').find(".insert-ajax-select");
-		var selectedValue = $(this).val();
-                var tdindex = [1,2,3,4]; 
-                if(selectedValue !='show_globally'){
+    var selectedValue = $(this).val();    
+    var tdindex = [1,2,3,4]; 
+
+    var date_opt    = '<option class="pt-child" value="before_published"> Before Published </option><option class="pt-child" value="after_published" selected=""> After Published </option>';
+    var regular_opt = '<option class="pt-child" value="equal"> Equal to </option><option class="pt-child" value="not_equal" selected=""> Not Equal to (Exclude) </option>';
+
+    if(selectedValue !='show_globally'){
                     
-                $.each(tdindex, function(i,e){  
-                    $(current_change).closest('tr').find('td').eq(e).show();  
-                 });    
+      $.each(tdindex, function(i,e){  
+          $(current_change).closest('tr').find('td').eq(e).show();  
+      });    
                 
 		var currentFiledNumber = $(this).attr("class").split(" ")[2];
-                var saswp_call_nonce = jQuery("#saswp_select_name_nonce").val();
-		
+    var saswp_call_nonce = jQuery("#saswp_select_name_nonce").val();
+    
+    if(selectedValue == 'date'){
+      current_change.parent().parent().find(".comparison").empty().append(date_opt);
+    }else{
+      current_change.parent().parent().find(".comparison").empty().append(regular_opt);
+    }
+    
 		parent.find(".ajax-output").remove();
 		parent.find(".select2-container").remove();
 		parent.find(".ajax-output-child").remove();
 		parent.find(".spinner").attr("style","visibility:visible");
 		parent.children(".spinner").addClass("show");
 		var ajaxURL = saswp_localize_data.ajax_url;
-                var group_index = jQuery(this).closest(".saswp-placement-group").attr('data-id'); 
+    var group_index = jQuery(this).closest(".saswp-placement-group").attr('data-id'); 
 		//ajax call
         $.ajax({
         url : ajaxURL,
@@ -143,7 +156,8 @@ jQuery(document).ready(function($){
       			parent.find(".spinner").attr("style","visibility:hidden").hide();
       			parent.append(data);
 				  taxonomyDataCall();
-				  saswp_select2();
+          saswp_select2();
+          saswp_schema_datepicker();
 				  parent.find(".ajax-output").change();
         },
         error: function(data){
@@ -208,7 +222,8 @@ function taxonomyDataCall(){
 	      			parentSelector.children(".spinner").removeClass("show");
 	      			parentSelector.find(".spinner").attr("style","visibility:hidden").hide();
 	      			parentSelector.append(data);					
-					saswp_select2();
+          saswp_select2();
+          saswp_schema_datepicker();
 	        },
 	        error: function(data){
 	          console.log("Failed Ajax Request");
