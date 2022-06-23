b860d3d271e74113f3fbf7db785bf9e1017609ed
Jeff McKenna <jmckenna@gatewaygeomatics.com>
2022-06-23 17:27:38 -0300

    add trailing slash to context url

--- admin_section/common-function.php
+++ admin_section/common-function.php
@@ -2675,10 +2675,10 @@ function saswp_on_activation(){
 
 function saswp_context_url(){
     
-    $url = 'http://schema.org';
+    $url = 'http://schema.org/';
     
     if(is_ssl()){
-        $url = 'https://schema.org';
+        $url = 'https://schema.org/';
     }
     
     return $url;
