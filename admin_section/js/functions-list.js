824a0b4410c83c9f9ae20a459c8f5953478ee035
Sanjeev Kumar <sanjeevsetu@gmail.com>
2022-12-10 12:32:50 +0530

    ..

--- admin_section/js/functions-list.js
+++ admin_section/js/functions-list.js
@@ -611,7 +611,7 @@
                                     if(j < explod[0]){
                                        // starating +='<span class="str-ic"></span>';   
                                         //starating +='<span class="saswp_star_color" style=color:'+color+'; >★</span>';   
-                                        starating +='<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="18px" viewBox="0 0 32 32"><defs><linearGradient id="grad"><stop offset="100%" class="saswp_star" stop-color='+color+' /><stop offset="100%" stop-color="grey"/></linearGradient></defs><path fill="url(#grad)" d="M20.388,10.918L32,12.118l-8.735,7.749L25.914,31.4l-9.893-6.088L6.127,31.4l2.695-11.533L0,12.118 l11.547-1.2L16.026,0.6L20.388,10.918z"/></svg>';
+                                        starating +='<span><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="18px" viewBox="0 0 32 32"><defs><linearGradient id="grad"><stop offset="100%" class="saswp_star" stop-color='+color+' /><stop offset="100%" stop-color="grey"/></linearGradient></defs><path fill="url(#grad)" d="M20.388,10.918L32,12.118l-8.735,7.749L25.914,31.4l-9.893-6.088L6.127,31.4l2.695-11.533L0,12.118 l11.547-1.2L16.026,0.6L20.388,10.918z"/></svg></span>';
 
                                     }else{
                                        // starating +='<span class="half-str"></span>';   
@@ -623,18 +623,18 @@
                                 }else{
                                     //starating +='<span class="str-ic"></span>';    
                                   //  starating +='<span class="saswp_star_color" style=color:'+color+';>★</span>';    
-                                    starating +='<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="18px" viewBox="0 0 32 32"><defs><linearGradient id="grad"><stop offset="100%" class="saswp_star" stop-color='+color+' /><stop offset="100%" stop-color="grey"/></linearGradient></defs><path fill="url(#grad)" d="M20.388,10.918L32,12.118l-8.735,7.749L25.914,31.4l-9.893-6.088L6.127,31.4l2.695-11.533L0,12.118 l11.547-1.2L16.026,0.6L20.388,10.918z"/></svg>';
+                                    starating +='<span><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="18px" viewBox="0 0 32 32"><defs><linearGradient id="grad"><stop offset="100%" class="saswp_star" stop-color='+color+' /><stop offset="100%" stop-color="grey"/></linearGradient></defs><path fill="url(#grad)" d="M20.388,10.918L32,12.118l-8.735,7.749L25.914,31.4l-9.893-6.088L6.127,31.4l2.695-11.533L0,12.118 l11.547-1.2L16.026,0.6L20.388,10.918z"/></svg></span>';
                                 }
 
                           } else{  
                                // starating +='<span class="df-clr"></span>';   
                                // starating +='<span class="saswp_star_color_gray">☆</span>';  
-                               starating +='<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="18px" viewBox="0 0 32 32"><defs><linearGradient id="grad"><stop offset="100%" stop-color="grey" /><stop offset="100%" stop-color="grey"/></linearGradient></defs><path fill="url(#grad)" d="M20.388,10.918L32,12.118l-8.735,7.749L25.914,31.4l-9.893-6.088L6.127,31.4l2.695-11.533L0,12.118 l11.547-1.2L16.026,0.6L20.388,10.918z"/></svg>';
+                               starating +='<span><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="18px" viewBox="0 0 32 32"><defs><linearGradient id="grad"><stop offset="100%" stop-color="grey" /><stop offset="100%" stop-color="grey"/></linearGradient></defs><path fill="url(#grad)" d="M20.388,10.918L32,12.118l-8.735,7.749L25.914,31.4l-9.893-6.088L6.127,31.4l2.695-11.533L0,12.118 l11.547-1.2L16.026,0.6L20.388,10.918z"/></svg></span>';
                                 
                           }                                                                                                                                
                         }
 
-                        starating += '</div>';
+                        starating += '<svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="200" height="200"> <defs> <linearGradient id="half_grad"> <stop offset="50%" stop-color="green"/> <stop offset="50%" stop-color="red" stop-opacity="1" /> </linearGradient> </defs> <path d="M20.388,10.918L32,12.118l-8.735,7.749L25.914, 31.4l-9.893-6.088L6.127,31.4l2.695-11.533L0, 12.118l11.547-1.2L16.026,0.6L20.388,10.918z" fill="url(#half_grad)"/></svg></div>';
                     starating += '</div>';
 
                     return starating;
