63de373726ec938ff17a45b6a7b0b9375174b102
Ravindra Vekariya <vekariyaravindra@gmail.com>
2022-12-26 10:29:38 +0530

    #1813

--- admin_section/common-function.php
+++ admin_section/common-function.php
@@ -3719,7 +3719,7 @@ function saswp_unique_multidim_array($array, $key) {
 function saswp_youtube_check_validate_url($yt_url) { 
     if(!empty($yt_url) && isset($yt_url)){
         $url_parsed_arr = parse_url($yt_url);
-        if ($url_parsed_arr['host'] == "youtu.be" || $url_parsed_arr['host'] == "www.youtube.com" || $url_parsed_arr['path'] == "/watch") {
+        if ($url_parsed_arr['host'] == "youtu.be" || $url_parsed_arr['host'] == "www.youtube.com" || $url_parsed_arr['path'] == "/watch" ) {
             return $yt_url;
         }else{
             return "";
