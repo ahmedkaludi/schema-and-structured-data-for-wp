78faf08c8d5fe52ca1052cb5d3326471a16e0235
shridhamdeveloper <shridham55@gmail.com>
2023-06-23 10:51:49 +0530

    #1959
    Resolved

--- modules/gutenberg/includes/class-gutenberg.php
+++ modules/gutenberg/includes/class-gutenberg.php
@@ -702,15 +702,12 @@ class SASWP_Gutenberg {
                         $className = 'class="'.esc_attr($attributes['className']).'"';
                     }
 
-                    if(!isset($attributes['listStyleType']))
-                    {
-                        $attributes['listStyleType']='none';
-                    }
+                    
 
                     if(($attributes['listStyleType']=='none')){
                         echo'<ol '.$className.' style="list-style-type:none;">';
-                     }elseif(($attributes['listStyleType']=='number')){
-                        echo '<ol>';
+                     }elseif(($attributes['listStyleType']=='disc')){
+                        echo'<ol '.$className.' style="list-style-type:disc;">';
                      }else{
                         echo '<ol>';
                      }
@@ -736,11 +733,11 @@ class SASWP_Gutenberg {
                     foreach($attributes['items'] as $item){
                        
                       if($item['title'] || $item['description']){
-                        echo '<li style="list-style-type: '.esc_attr($attributes['headingTag']).'">'; 
+                        echo '<li>'; 
                         if(!empty($attributes['headingTag']) && $attributes['headingTag'] == 'h1' || $attributes['headingTag'] == 'h2' || $attributes['headingTag'] == 'h3' || $attributes['headingTag'] == 'h4' || $attributes['headingTag'] == 'h5' || $attributes['headingTag'] == 'h6'){
                             echo '<'.html_entity_decode(esc_attr($attributes['headingTag'])).'>'. html_entity_decode(esc_attr($item['title'])).'</'.html_entity_decode(esc_attr($attributes['headingTag'])).'>';
                         }else{
-                            echo '<strong class="saswp-how-to-step-name">'. html_entity_decode(esc_attr($item['title'])).'</strong>';
+                            echo '<h1 class="saswp-how-to-step-name">'. html_entity_decode(esc_attr($item['title'])).'</h1>';
                         }
                         echo '<p class="saswp-how-to-step-text">'.html_entity_decode(esc_textarea($item['description'])).'</p>';
                         echo '</li>';
