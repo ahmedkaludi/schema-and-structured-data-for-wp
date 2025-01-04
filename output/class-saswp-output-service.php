2e8d255f70efd817a7dd42ed91bd5bcdc015ff19
shridhamdeveloper <38998323+shridhamdeveloper@users.noreply.github.com>
2025-01-04 16:45:25 +0530

    #2236

--- output/class-saswp-output-service.php
+++ output/class-saswp-output-service.php
@@ -3116,7 +3116,7 @@ Class SASWP_Output_Service{
                             if ( ! empty( $local_offer ) && is_array( $local_offer ) ) {
                                 $make_offer                   =   array();
                                 $make_offer['@type']          =   'Offer';
-                                $make_offer['@id']            =   '#service'. $lmo_key + 1;
+                                $make_offer['@id']            =   '#service'. ( $lmo_key + 1 );
                                 $make_offer['itemOffered']    =   $local_offer;
 
                                 $input1['makesOffer'][]       =   $make_offer;    
