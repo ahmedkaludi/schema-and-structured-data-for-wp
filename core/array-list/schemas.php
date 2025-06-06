843f6bddfb84f22d89454ec665b96548353e3751
shridhamdeveloper <38998323+shridhamdeveloper@users.noreply.github.com>
2025-06-06 17:45:53 +0530

    #2294

--- core/array-list/schemas.php
+++ core/array-list/schemas.php
@@ -62,6 +62,7 @@ return array(
             'Event'                    => 'Event',
         ),
         'Game' =>  array(
+             'Game'                     => 'Game',
              'VideoGame'                => 'VideoGame'                                         
          ),
         'Intangible' => array(
