acaad876357deab4e433f945aa3ff8e5eff79421
Mohammed Qutubuddin Nasir <muhammed.qutubuddin786@gmail.com>
2026-02-17 18:07:50 +0530

    #2407
    Improvement done

--- output/class-saswp-output-service.php
+++ output/class-saswp-output-service.php
@@ -8079,8 +8079,14 @@ Class SASWP_Output_Service{
                      $input1['validThrough'] =    $custom_fields['saswp_jobposting_schema_validthrough'];
                     }
                     if ( isset( $custom_fields['saswp_jobposting_schema_employment_type']) ) {
-
-                     $input1['employmentType'] =    explode( ',', $custom_fields['saswp_jobposting_schema_employment_type'] );
+                        
+                        $raw_employment_type = isset($custom_fields['saswp_jobposting_schema_employment_type']) ? $custom_fields['saswp_jobposting_schema_employment_type'] : '';
+                        
+                        if ( is_array( $raw_employment_type ) ) {
+                            $input1['employmentType'] = $raw_employment_type;
+                        } else {
+                            $input1['employmentType'] = explode( ',', (string) $raw_employment_type );
+                        }
                     }
                     if ( isset( $custom_fields['saswp_jobposting_schema_jobimmediatestart']) ) {
                      $input1['jobImmediateStart'] =    $custom_fields['saswp_jobposting_schema_jobimmediatestart'];
